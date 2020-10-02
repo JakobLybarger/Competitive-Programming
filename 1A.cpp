e2cf34cd3f3e3bc0a81f39a0c133d8f286beba1e
Jakob Lybarger <jakoblybarger911@gmail.com>
2020-10-02 00:07:43 -0500

    Codeforces 1A, difficulty 1000

---
+++ 1A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long n, m, a, x, y;
+    cin >> n >> m >> a;
+    if(m%a==0) {
+        x=m/a;
+    }
+    else {
+        x=m/a + 1;
+    }
+
+    if (n%a==0) {
+        y=n/a;
+    }
+    else {
+        y=n/a+1;
+    }
+
+    cout << x * y;
+    return 0;
+}
