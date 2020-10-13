779aa615619fbff960a6b525dfa64817b6fbe3ed
Jakob Lybarger <jakoblybarger911@gmail.com>
2020-10-12 22:08:39 -0500

    LeetCode SymmetricTree and deletions

---
+++ SymmetricTree.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ //Definition for a binary tree node.
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        
+        return test(root->left, root->right);
+    }
+    
+    bool test(TreeNode* left, TreeNode* right) {
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        if(left->val != right->val) return false;
+        return test(left->left, right->right) && test(left->right, right->left);
+    }
+};
